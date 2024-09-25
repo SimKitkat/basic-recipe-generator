64506f1e22ac686b1010ad1528d98fd47393a7fc
SimKitkat <simone.cornelius14@gmail.com>
2024-09-25 15:58:48 +0200

    api integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(Response) {
   new Typewriter(".recipe-body", {
-    strings: "Generating recipe",
+    strings: Response.data.answer,
     autoStart: true,
     dalay: 1,
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let recipeInput = document.querySelector("#recipe-input");
+  let apiKey = "8b0a6ad4348ba4af0cb7ofe7df4t6b19";
+  let apiContext =
+    "You are a master baker who specializes in baking cookies. You are required to make a step-by-step basic cookie recipe by Include a list of ingredients, measurements, and detailed instructions.display the recipe in basic HTML format and seperate each line with a <br />. Do not include the prompt in your response.Make sure to use the user ingredient. Sign the recipe wit `SheCodes AI`";
+  let apiPrompt = `user ingredient: ${recipeInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${apiPrompt}&context=${apiContext}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeForm = document.querySelector(".recipe");
