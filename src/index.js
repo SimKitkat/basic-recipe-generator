9465031ef27e373e22576b822c55d5e5c6d08a59
SimKitkat <simone.cornelius14@gmail.com>
2024-09-25 15:13:50 +0200

    added basic js and typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert(hello);
+function generateRecipe(event) {
+  event.preventDefault();
+
+  new Typewriter(".recipe-body", {
+    strings: "Generating recipe",
+    autoStart: true,
+    dalay: 1,
+  });
+}
+
+let recipeForm = document.querySelector(".recipe");
+recipeForm.addEventListener("submit", generateRecipe);
