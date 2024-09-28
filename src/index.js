b2e4400031c2080ffe8958ca4b422f72c80772e9
SimKitkat <simone.cornelius14@gmail.com>
2024-09-28 11:35:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generateRecipe(event) {
   let recipeInput = document.querySelector("#recipe-input");
   let apiKey = "8b0a6ad4348ba4af0cb7ofe7df4t6b19";
   let apiContext =
-    "You are a master baker who specializes in baking cookies. You are required to make a step-by-step basic cookie recipe by Include a list of ingredients, measurements, and detailed instructions.display the recipe in basic HTML format and seperate each line with a <br />. Do not include the prompt and the word `html` in your response. Make sure to use the user ingredient. Sign the recipe wit `SheCodes AI`";
+    "You are a master baker who specializes in baking cookies. You are required to make a step-by-step basic cookie recipe by Include a list of ingredients, measurements, and simple instructions 5 lines or less.display the recipe in basic HTML format and seperate each line with a <br />. Do not include the prompt and the word `html` in your response. Make sure to use the user ingredient. Sign the recipe wit `SheCodes AI`";
   let apiPrompt = `user ingredient: ${recipeInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${apiPrompt}&context=${apiContext}&key=${apiKey}`;
 
